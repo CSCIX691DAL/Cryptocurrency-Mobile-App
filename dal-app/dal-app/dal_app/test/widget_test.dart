4ef2235ad5f6a24893051af38d45b1f7abe7eeae
Noah Cormier-Ratajczak <nh471300@dal.ca>
2021-06-02 14:31:38 -0300

    Project structure changes

--- dal-app/dal-app/dal_app/test/widget_test.dart
+++ dal-app/dal-app/dal_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dal_app/Main%20Interface/main.dart';
+import 'package:dal_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
